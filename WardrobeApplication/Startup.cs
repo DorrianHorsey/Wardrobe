108e0f08628207b4843d4be2464b550976ee5b14
DorrianHorsey <deardorrian@gmail.com>
2017-05-01 14:18:10 -0400

    first commit

---
+++ WardrobeApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WardrobeApplication.Startup))]
+namespace WardrobeApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
